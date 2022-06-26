5bc176d526a804f3540b457942923d0490e94841
Pedro <pedroparro10@gmail.com>
2022-06-26 19:05:10 -0300

    Cadastro Access - C#

---
+++ Cadastro/Cadastro/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace Cadastro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pessoasBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.databaseDataSet = new Cadastro.DatabaseDataSet();
+            this.pessoasTableAdapter = new Cadastro.DatabaseDataSetTableAdapters.PessoasTableAdapter();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Anterior = new System.Windows.Forms.Button();
+            this.Proximo = new System.Windows.Forms.Button();
+            this.Deletar = new System.Windows.Forms.Button();
+            this.NovaPessoa = new System.Windows.Forms.Button();
+            this.Salvar = new System.Windows.Forms.Button();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nOMEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sOBRENOMEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nASCIMENTODataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.eMAILDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cPFDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pessoasBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.nOMEDataGridViewTextBoxColumn,
+            this.sOBRENOMEDataGridViewTextBoxColumn,
+            this.nASCIMENTODataGridViewTextBoxColumn,
+            this.eMAILDataGridViewTextBoxColumn,
+            this.cPFDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.pessoasBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(727, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // pessoasBindingSource
+            // 
+            this.pessoasBindingSource.DataMember = "Pessoas";
+            this.pessoasBindingSource.DataSource = this.databaseDataSet;
+            // 
+            // databaseDataSet
+            // 
+            this.databaseDataSet.DataSetName = "DatabaseDataSet";
+            this.databaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // pessoasTableAdapter
+            // 
+            this.pessoasTableAdapter.ClearBeforeFill = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.maskedTextBox2);
+            this.groupBox1.Controls.Add(this.maskedTextBox1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 215);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(737, 151);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cadastro de Pessoas";
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pessoasBindingSource, "CPF", true));
+            this.maskedTextBox2.Location = new System.Drawing.Point(76, 122);
+            this.maskedTextBox2.Mask = "000.000.000\\-00";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(82, 20);
+            this.maskedTextBox2.TabIndex = 9;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pessoasBindingSource, "NASCIMENTO", true));
+            this.maskedTextBox1.Location = new System.Drawing.Point(76, 70);
+            this.maskedTextBox1.Mask = "00/00/0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(65, 20);
+            this.maskedTextBox1.TabIndex = 5;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "CPF";
+            // 
+            // textBox4
+            // 
+            this.textBox4.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pessoasBindingSource, "EMAIL", true));
+            this.textBox4.Location = new System.Drawing.Point(76, 96);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(172, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nascimento";
+            // 
+            // textBox2
+            // 
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pessoasBindingSource, "SOBRENOME", true));
+            this.textBox2.Location = new System.Drawing.Point(76, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(172, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sobrenome";
+            // 
+            // textBox1
+            // 
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pessoasBindingSource, "NOME", true));
+            this.textBox1.Location = new System.Drawing.Point(76, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // Anterior
+            // 
+            this.Anterior.Location = new System.Drawing.Point(13, 169);
+            this.Anterior.Name = "Anterior";
+            this.Anterior.Size = new System.Drawing.Size(75, 40);
+            this.Anterior.TabIndex = 1;
+            this.Anterior.Text = "Anterior";
+            this.Anterior.UseVisualStyleBackColor = true;
+            this.Anterior.Click += new System.EventHandler(this.Anterior_Click);
+            // 
+            // Proximo
+            // 
+            this.Proximo.Location = new System.Drawing.Point(94, 169);
+            this.Proximo.Name = "Proximo";
+            this.Proximo.Size = new System.Drawing.Size(75, 40);
+            this.Proximo.TabIndex = 2;
+            this.Proximo.Text = "Próximo";
+            this.Proximo.UseVisualStyleBackColor = true;
+            this.Proximo.Click += new System.EventHandler(this.Proximo_Click);
+            // 
+            // Deletar
+            // 
+            this.Deletar.Location = new System.Drawing.Point(496, 170);
+            this.Deletar.Name = "Deletar";
+            this.Deletar.Size = new System.Drawing.Size(75, 40);
+            this.Deletar.TabIndex = 3;
+            this.Deletar.Text = "Deletar";
+            this.Deletar.UseVisualStyleBackColor = true;
+            this.Deletar.Click += new System.EventHandler(this.Deletar_Click);
+            // 
+            // NovaPessoa
+            // 
+            this.NovaPessoa.Location = new System.Drawing.Point(577, 170);
+            this.NovaPessoa.Name = "NovaPessoa";
+            this.NovaPessoa.Size = new System.Drawing.Size(81, 40);
+            this.NovaPessoa.TabIndex = 4;
+            this.NovaPessoa.Text = "Nova Pessoa";
+            this.NovaPessoa.UseVisualStyleBackColor = true;
+            this.NovaPessoa.Click += new System.EventHandler(this.NovaPessoa_Click);
+            // 
+            // Salvar
+            // 
+            this.Salvar.Location = new System.Drawing.Point(664, 169);
+            this.Salvar.Name = "Salvar";
+            this.Salvar.Size = new System.Drawing.Size(75, 40);
+            this.Salvar.TabIndex = 5;
+            this.Salvar.Text = "Salvar";
+            this.Salvar.UseVisualStyleBackColor = true;
+            this.Salvar.Click += new System.EventHandler(this.Salvar_Click);
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            // 
+            // nOMEDataGridViewTextBoxColumn
+            // 
+            this.nOMEDataGridViewTextBoxColumn.DataPropertyName = "NOME";
+            this.nOMEDataGridViewTextBoxColumn.HeaderText = "NOME";
+            this.nOMEDataGridViewTextBoxColumn.Name = "nOMEDataGridViewTextBoxColumn";
+            // 
+            // sOBRENOMEDataGridViewTextBoxColumn
+            // 
+            this.sOBRENOMEDataGridViewTextBoxColumn.DataPropertyName = "SOBRENOME";
+            this.sOBRENOMEDataGridViewTextBoxColumn.HeaderText = "SOBRENOME";
+            this.sOBRENOMEDataGridViewTextBoxColumn.Name = "sOBRENOMEDataGridViewTextBoxColumn";
+            // 
+            // nASCIMENTODataGridViewTextBoxColumn
+            // 
+            this.nASCIMENTODataGridViewTextBoxColumn.DataPropertyName = "NASCIMENTO";
+            this.nASCIMENTODataGridViewTextBoxColumn.HeaderText = "NASCIMENTO";
+            this.nASCIMENTODataGridViewTextBoxColumn.Name = "nASCIMENTODataGridViewTextBoxColumn";
+            // 
+            // eMAILDataGridViewTextBoxColumn
+            // 
+            this.eMAILDataGridViewTextBoxColumn.DataPropertyName = "EMAIL";
+            this.eMAILDataGridViewTextBoxColumn.HeaderText = "EMAIL";
+            this.eMAILDataGridViewTextBoxColumn.Name = "eMAILDataGridViewTextBoxColumn";
+            this.eMAILDataGridViewTextBoxColumn.Width = 190;
+            // 
+            // cPFDataGridViewTextBoxColumn
+            // 
+            this.cPFDataGridViewTextBoxColumn.DataPropertyName = "CPF";
+            this.cPFDataGridViewTextBoxColumn.HeaderText = "CPF";
+            this.cPFDataGridViewTextBoxColumn.Name = "cPFDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 371);
+            this.Controls.Add(this.Salvar);
+            this.Controls.Add(this.NovaPessoa);
+            this.Controls.Add(this.Deletar);
+            this.Controls.Add(this.Proximo);
+            this.Controls.Add(this.Anterior);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pessoasBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DatabaseDataSet databaseDataSet;
+        private System.Windows.Forms.BindingSource pessoasBindingSource;
+        private DatabaseDataSetTableAdapters.PessoasTableAdapter pessoasTableAdapter;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Anterior;
+        private System.Windows.Forms.Button Proximo;
+        private System.Windows.Forms.Button Deletar;
+        private System.Windows.Forms.Button NovaPessoa;
+        private System.Windows.Forms.Button Salvar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nOMEDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sOBRENOMEDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nASCIMENTODataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn eMAILDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cPFDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Cadastro/Cadastro/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cadastro
+{
+    public partial class Form1 : Form
+    {
+        public DatabaseDataSet.PessoasDataTable DataBaseDataSet { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'databaseDataSet.Pessoas' table. You can move, or remove it, as needed.
+            this.pessoasTableAdapter.Fill(this.databaseDataSet.Pessoas);
+
+        }
+
+        private void Anterior_Click(object sender, EventArgs e)
+        {
+            pessoasBindingSource.MovePrevious();
+        }
+
+        private void Proximo_Click(object sender, EventArgs e)
+        {
+            pessoasBindingSource.MoveNext();
+        }
+
+        private void Deletar_Click(object sender, EventArgs e)
+        {
+            pessoasBindingSource.RemoveCurrent();
+        }
+
+        private void NovaPessoa_Click(object sender, EventArgs e)
+        {
+            pessoasBindingSource.AddNew();
+        }
+
+        private void Salvar_Click(object sender, EventArgs e)
+        {
+            pessoasBindingSource.EndEdit();
+            pessoasTableAdapter.Update(databaseDataSet);
+        }
+    }
+}
